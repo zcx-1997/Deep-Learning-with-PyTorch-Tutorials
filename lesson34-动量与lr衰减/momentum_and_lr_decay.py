657ea5715f13445234507f9b4538536a7d9326a1
zcx <zcx_demail@163.com>
2021-09-30 21:53:04 +0800

    lesson35

---
+++ lesson34-动量与lr衰减/momentum_and_lr_decay.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time : 2021/9/30
+@Author : Lenovo
+@File : momentum_and_lr_decay
+@Description : 
+"""
+import  torch
+import  torch.nn as nn
+import  torch.nn.functional as F
+import  torch.optim as optim
+from    torchvision import datasets, transforms
+
+
+batch_size=200
+learning_rate=0.01
+epochs=10
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       # transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False, transform=transforms.Compose([
+        transforms.ToTensor(),
+        # transforms.Normalize((0.1307,), (0.3081,))
+    ])),
+    batch_size=batch_size, shuffle=True)
+
+
+
+class MLP(nn.Module):
+
+    def __init__(self):
+        super(MLP, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(784, 200),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(200, 200),
+            nn.LeakyReLU(inplace=True),
+            nn.Linear(200, 10),
+            nn.LeakyReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+
+        return x
+
+device = torch.device('cuda:0')
+net = MLP().to(device)
+
+optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.01, weight_decay=0.01)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1*300, gamma=0.1)
+# scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer,'min')
+
+criteon = nn.CrossEntropyLoss().to(device)
+
+
+global_step = 0
+
+for epoch in range(epochs):
+    # 打印当前学习率
+    print(optimizer.state_dict()['param_groups'][0]['lr'])
+
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data = data.view(-1, 28*28)
+        data, target = data.to(device), target.cuda()
+
+        logits = net(data)
+        loss = criteon(logits, target)
+
+        optimizer.zero_grad()
+        loss.backward()
+        # print(w1.grad.norm(), w2.grad.norm())
+        optimizer.step()
+        scheduler.step()
+
+        # scheduler.step(loss)
+
+
+        global_step += 1
+
+
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        data = data.view(-1, 28 * 28)
+        data, target = data.to(device), target.cuda()
+        logits = net(data)
+        test_loss += criteon(logits, target).item()
+
+        pred = logits.argmax(dim=1)
+        correct += pred.eq(target).float().sum().item()
+
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
